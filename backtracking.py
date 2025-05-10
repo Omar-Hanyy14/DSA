f1578793e3bfc9b5c8101369f12a50120edc1126
Omar Hany <ben_hany@Omars-MacBook-Pro.local>
2025-05-10 18:27:37 +0300

    leaf path construction

--- backtracking.py
+++ backtracking.py
@@ -15,3 +15,15 @@ def canReachLeaf(root):
     if canReachLeaf(root.right):
         return True
     return False
+
+def leafPath(root, path):
+    if not root or root.val == 0:
+        return False
+    if not root.left and not root.right:
+        return True
+    if leafPath(root.left, path):
+        return True
+    if leafPath(root.right, path):
+        return True
+    path.pop()
+    return False
